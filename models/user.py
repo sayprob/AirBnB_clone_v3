b59e077c5d6c8b75737a1475298aa3dd6310dcb2
AITBENADDI Youssef <youssefaitbenaddi8@gmail.com>
2024-04-02 00:23:12 +0000

    Add files via upload

---
+++ models/user.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" holds class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from hashlib import md5
+
+
+class User(BaseModel, Base):
+    """Representation of a user """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
+
+    """
+    Security is VERY important and storing passwords in plain text is
+    a horible idea.  This update incorporates md5, which isn't perfect
+    but it's better than nothing
+    """
+    def __setattr__(self, key, value):
+        """set encrypted password for users"""
+        if key == 'password':
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(key, value)
